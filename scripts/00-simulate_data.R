9b6afdf1b7bf80fb46f606754374c7152877092a
Yun Chu <yun.chu@mail.utoronto.ca>
2024-09-21 17:44:11 -0400

    simulate the sales data with Poisson distribution

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,38 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates Toronto Islands Ferry Tickets Sales Count Data
+# Author: Yun Chu
+# Date: 21 September 2024
+# Contact: yun.chu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: None
+# Any other information needed? None
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(200)
 
+# Define the start and end time
+start_date <- as.Date("2023-08-01")
+end_date <- as.Date("2024-08-31")
 
+#Set the number of Randoom dates
+number_of_dates <- 200
 
+data <- 
+  tibble(
+    dates = as.Date(
+      runif(
+        n = number_of_dates,
+        min = as.numeric(start_date),
+        max = as.numeric(end_date)
+      ),
+      origin = "1970-01-01"
+    ),
+    number_of_sales = rpois(n = number_of_dates, lambda = 15)
+  )
+
+# Write_csv
+write_csv(data, file = "data/raw_data/simulated.csv")
